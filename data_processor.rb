23faf8ab6a8f737bb01933397f346bd61649bdc8
Alexis Chavez <aalexis.chavez@gmail.com>
2024-09-05 00:03:35 -0600

    Challenge completed

---
+++ data_processor.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module DataProcessor
+  attr_reader :file_path, :data
+
+  def initialize(file_path:)
+    @file_path = file_path
+    @data = []
+    read_data
+  end
+
+  def read_data
+    File.open(file_path, 'r') do |file|
+      file.each_line.with_index do |line, index|
+        next if index < self.class::SKIP_LINES_COUNT
+
+        process_line(line)
+      end
+    end
+  end
+
+  private
+
+  def process_line(line)
+    raise NotImplementedError, "You must implement #{self.class}##{__method__}"
+  end
+end
